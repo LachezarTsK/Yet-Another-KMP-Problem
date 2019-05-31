98319b458c993ed4c5d4836c11c1ebb897b795ac
Lachezar Kutsarov <lkutsarov@gmail.com>
2019-05-31 11:39:55 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -5,11 +5,6 @@ public class Solution {
 	private static int index_minFrequency;
 	private static int index_firstMinValueChar;
 	private static int index_secondMinValueChar;
-	/**
-	 * Decimal value of character "z" as per ASCII Table.
-	 */
-	private static int first_minValueChar = 122;
-	private static int second_minValueChar = 122;
 
 	public static void main(String[] arg) {
 		Scanner scanner = new Scanner(System.in);
@@ -23,11 +18,20 @@ public class Solution {
 	}
 
 	/**
-	 * The method finds the values/indexes of the lexicographically smallest and
-	 * second smallest characters as well as the frequency/index of the character
-	 * with the smallest number of occurrence.
+	 * The method finds the indexes of the lexicographically smallest and second
+	 * smallest characters as well as the frequency and index of the character with
+	 * the smallest number of occurrence. The corresponding instance variables are
+	 * modified accordingly.
+	 * 
+	 * Since the 26 values in the input array represent the frequency of the
+	 * alphabet characters, aligned in lexicographically ascending order, the first
+	 * and second non-zero values represent the smallest and second smallest
+	 * lexicographical characters.
 	 */
 	private static void findKeyValuesAndIndexes(int[] inputArray) {
+
+		boolean firstNonZeroValueReached = false;
+		boolean secondNonZeroValueReached = false;
 
 		for (int i = 0; i < inputArray.length; i++) {
 
@@ -37,13 +41,13 @@ public class Solution {
 					minFrequency = inputArray[i];
 					index_minFrequency = i;
 				}
-				if (97 + i <= first_minValueChar) {
-					first_minValueChar = 97 + i;
+
+				if (firstNonZeroValueReached == false) {
 					index_firstMinValueChar = i;
-				}
-				if (97 + i > first_minValueChar && 97 + i <= second_minValueChar) {
-					second_minValueChar = 97 + i;
+					firstNonZeroValueReached = true;
+				} else if (firstNonZeroValueReached == true && secondNonZeroValueReached == false) {
 					index_secondMinValueChar = i;
+					secondNonZeroValueReached = true;
 				}
 			}
 		}
@@ -52,23 +56,43 @@ public class Solution {
 	/**
 	 * The method finds the string configuration that would have a minimum sum of
 	 * the values contained in Longest Prefix Suffix Array of Knuth–Morris–Pratt
-	 * Algorithm.
+	 * Algorithm and has lexicographically smallest value for this minimum sum.
 	 * 
-	 * If more than one string configuration has this minimum sum, then the string
-	 * with the smallest lexicographical value is returned.
+	 * The method has the following steps:
 	 * 
-	 * @return A string corresponding to the above-mentioned conditions.
+	 * 1. If minimum frequency is greater than 2 and the character with minimum
+	 * frequency has also the smallest lexicographical value, and there are more
+	 * than two different characters, then:
+	 * 
+	 * 1.1 Put two "minimum frequency characters" at the front of the string.
+	 * 
+	 * 1.2 While there are still remaining "minimum frequency characters",
+	 * alternately append one "second smallest character" and one "minimum frequency
+	 * character".
+	 * 
+	 * 1.3 Append the rest of the characters as they occur in the input array.
+	 * 
+	 * 2. If the conditions in step 1 are not met, then:
+	 * 
+	 * 2.1 Put one "minimum frequency character" at the front of the string.
+	 * 
+	 * 2.2 Append the rest of the characters as they occur in the input array.
+	 * 
+	 * @return A string with minimum sum of the values in Longest Prefix Suffix
+	 *         Array and with the smallest lexicographical value for this minimum
+	 *         sum.
 	 */
 	private static String findString_MinimumKMPandLexicographicallySmallest(int[] inputArray) {
 		findKeyValuesAndIndexes(inputArray);
 		StringBuilder minimum = new StringBuilder();
 
-		if (minFrequency >= 2 && index_minFrequency == index_firstMinValueChar) {
+		if (minFrequency >= 2 && index_minFrequency == index_firstMinValueChar && index_secondMinValueChar != 0) {
+
 			minimum.append((char) (97 + index_minFrequency));
 			minimum.append((char) (97 + index_minFrequency));
 			inputArray[index_minFrequency] -= 2;
 
-			while (inputArray[index_secondMinValueChar] > 0 && inputArray[index_minFrequency] > 0) {
+			while (inputArray[index_minFrequency] > 0) {
 
 				minimum.append((char) (97 + index_secondMinValueChar));
 				inputArray[index_secondMinValueChar]--;
